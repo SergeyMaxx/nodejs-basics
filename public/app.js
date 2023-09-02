e5fd3313c5114d9014de2aab0d8e277bc3fc138b
Sergey Maximov <95900933+SergeyMaxx@users.noreply.github.com>
2023-09-02 17:35:35 +0300

    Модуль 3. Backend Дополнительные задания недели Задание #2

--- public/app.js
+++ public/app.js
@@ -4,14 +4,42 @@ document.addEventListener('click', async ({target}) => {
       .then(() => target.closest('li').remove())
   }
 
-  if (target.dataset.type === 'update') {
-    const newTitle = prompt('Update note')
+  const listItem = target.closest('li')
+  const editBtn = listItem.querySelector('.edit-btn')
+  const saveBtn = listItem.querySelector('.save-btn')
+  const cancelBtn = listItem.querySelector('.cancel-btn')
+  const removeBtn = listItem.querySelector('.btn-danger')
+  const title = listItem.querySelector('[data-type="title"]')
 
-    if (newTitle.trim()) {
-      update(target.dataset.id, newTitle).then(() => {
-        target.closest('li').querySelector('div').innerText = newTitle
-      })
-    }
+  if (target.dataset.type === 'edit') {
+    target.style.display = 'none'
+    removeBtn.style.display = 'none'
+    saveBtn.style.display = 'block'
+    cancelBtn.style.display = 'block'
+    title.dataset.originalTitle = title.innerText
+    title.style.minWidth = '100px'
+    title.contentEditable = 'true'
+    title.focus()
+  }
+
+  if (target.dataset.type === 'save') {
+    target.style.display = 'none'
+    cancelBtn.style.display = 'none'
+    listItem.querySelector('[data-type="edit"]').style.display = 'block'
+    removeBtn.style.display = 'block'
+    title.contentEditable = 'false'
+    const newTitle = title.innerText
+
+    await update(target.dataset.id, newTitle).then(() => title.innerText = newTitle)
+  }
+
+  if (target.dataset.type === 'cancel') {
+    target.style.display = 'none'
+    editBtn.style.display = 'none'
+    title.innerText = title.dataset.originalTitle
+    title.contentEditable = 'false'
+    listItem.querySelector('[data-type="edit"]').style.display = 'block'
+    removeBtn.style.display = 'block'
   }
 })
 
